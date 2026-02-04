577550902c08fa62cb9c48c90052f27feacaa788
Yu Chen Liu <vincent6786@users.noreply.github.com>
2026-02-04 08:48:16 +0800

    Update awos.js

--- api/awos.js
+++ api/awos.js
@@ -1,14 +1,13 @@
-// Secure proxy for HTTP content
 export default async function handler(req, res) {
-    const targetUrl = 'http://kmhr.awosnet.com/text.php'; // Targeting the text version directly
+    // This allows your HTTPS app to read the HTTP KMHR site securely
+    const targetUrl = 'http://kmhr.awosnet.com/text.php'; 
 
     try {
         const response = await fetch(targetUrl);
         const html = await response.text();
-
-        // Add a base tag so relative links/images in the HTML still work
+        // Fix relative links so they don't break
         const modifiedHtml = html.replace('<head>', '<head><base href="http://kmhr.awosnet.com/">');
-
+        
         res.setHeader('Content-Type', 'text/html');
         return res.status(200).send(modifiedHtml);
     } catch (error) {
